b6b57e13f649aaead81b9482fd17ead8db567e67
Lakmal Perera <lakmettel@gmail.com>
2026-02-17 11:53:45 +0530

    Fix Railway deploy: static files, port, yt-dlp

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 const DOWNLOAD_DIR = path.join(__dirname, "downloads");
 
 if (!fs.existsSync(DOWNLOAD_DIR)) {
@@ -30,6 +32,10 @@ app.get("/progress", (req, res) => {
     clients = clients.filter(c => c !== res);
   });
 });
+
+
+
+
 
 function broadcast(data) {
   clients.forEach(res => {
@@ -120,6 +126,8 @@ app.get("/file/:id", (req, res) => {
 });
 
 /* ================= START ================= */
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
